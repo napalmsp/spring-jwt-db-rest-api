28423daabfff854d57e147fa7df1a98d94ed95fd
Gleyson <gzynho@hotmail.com>
2019-03-06 15:33:10 -0300

    Gerando arquivo war

--- src/main/java/open/digytal/WebApiApplication.java
+++ src/main/java/open/digytal/WebApiApplication.java
@@ -3,12 +3,13 @@ package open.digytal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
-public class WebApiApplication {
+public class WebApiApplication extends SpringBootServletInitializer { // extends War File
 	public static void main(String[] args) {
 		SpringApplication.run(WebApiApplication.class, args);
 	}
