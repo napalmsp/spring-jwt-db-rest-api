cb8697c7ab8e9ef9aaa892480f4142a15b7daa6d
Gleyson <gzynho@hotmail.com>
2019-03-06 18:05:37 -0300

    configurando jar file - deploy

--- src/main/java/open/digytal/WebApiApplication.java
+++ src/main/java/open/digytal/WebApiApplication.java
@@ -3,13 +3,12 @@ package open.digytal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
-public class WebApiApplication extends SpringBootServletInitializer { // extends War File
+public class WebApiApplication {// WAR --> extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApiApplication.class, args);
 	}
